8bba07c1ea0b5bac21cb385df83e204d16850d61
squalonero <mirco.cucca@gmail.com>
2022-08-22 20:11:56 +0200

    Fix app instantiate

--- src/realmApp.js
+++ src/realmApp.js
@@ -3,8 +3,6 @@ import Realm from 'realm';
 // Invokes the shared instance of the Realm app.
 // TODO: Create a Realm App instance with your Realm app ID.
 
-const app = new Realm({
-  app: 'tasktracker-blema',
-});
+const app = new Realm.App({id: 'tasktracker-blema'});
 
 export default app;
