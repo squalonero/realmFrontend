29bf52b96a2c452e140c18642da0a513944c9e2b
squalonero <mirco.cucca@gmail.com>
2022-08-22 17:56:15 +0200

    Fisrt

---
+++ src/realmApp.js
@@ -0,0 +1,10 @@
+import Realm from 'realm';
+
+// Invokes the shared instance of the Realm app.
+// TODO: Create a Realm App instance with your Realm app ID.
+
+const app = new Realm({
+  app: 'tasktracker-blema',
+});
+
+export default app;
